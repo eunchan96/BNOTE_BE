1ed373d6e01d61e324c70e69487f103e7e7a82d9
eunchan96 <taegwon02@gmail.com>
2026-08-03 15:01:00 +0900

    Initial commit

---
+++ src/test/java/com/bnote/BnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BnoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
